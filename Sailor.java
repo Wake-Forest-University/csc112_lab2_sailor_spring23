3c4bcbc44996c73867e76c89f1c744002e2d95f7
Errin Fulp <fulp@wfu.edu>
2023-01-20 18:54:31 -0500

    Initial commit

---
+++ Sailor.java
@@ -0,0 +1,23 @@
+/**
+ * This program simulates a drunken stagger on a board (plank) and
+ * estimates the probability of falling.
+ *
+ * @author Nirre Pluf {@literal <pluf@wfu.edu>}
+ * @version 0.1, Jan. 20, 2023
+ */
+
+import java.util.Random;
+
+public class Sailor {
+    public static void main(String[] args) {  
+        var rand = new Random(112);
+
+	}
+
+
+    public static int randStep(Random rand)  
+    {  return (rand.nextInt(2) == 0 ? -1 : 1);  }  
+
+}
+
+
